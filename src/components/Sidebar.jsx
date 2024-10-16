8744540ca04007791b29ad43b321f297cc2e5eb7
Cesar <cesarnsingi@hotmail.com>
2024-10-16 16:09:28 +0400

    design sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,12 @@
-/* import React from 'react'
-import {assets} from "../assets/assets"
+import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-        <div className='bg-[#121212] h-[15%] round flex flex-col justify-around'>
-         <div className= 'flex items-center gap-3 pl-8 cursor-pointer'>
-            <img src="{assets.home_icon" alt="" />
-            <p className='font-bold'>Home</p>
-     </div>
-        </div>
- </div>
-
+    <div>
+      
+    </div>
   )
 }
 
-export default Sidebar */
+export default Sidebar
+
