c024b17d99d43c40456609fafeb2f833ff33afef
Cesar <cesarnsingi@hotmail.com>
2024-10-17 13:22:10 +0400

    set AlbumItem

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AlbumItem = ({image,name,desc,id}) => {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <img src="" alt="" />
+      
+    </div>
+  )
+}
+
+export default AlbumItem
