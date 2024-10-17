ddc0afa12751a040b3fa89aaa545b27f8ed50be7
Cesar <cesarnsingi@hotmail.com>
2024-10-17 22:46:41 +0400

    add functionality to AlbumItem

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AlbumItem = ({image,name,desc,id}) => {
+
+    const navigate = useNavigate()
+
+    
   return (
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className='rounded' src={image} alt="" />
