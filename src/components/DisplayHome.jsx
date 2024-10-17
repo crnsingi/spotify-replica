b5750a2b929e71c5b6f95c51ab66e8a5bfd5fadb
Cesar <cesarnsingi@hotmail.com>
2024-10-17 16:21:03 +0400

    add more content

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -11,12 +11,14 @@ const DisplayHome = () => {
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
         <div className='flex overflow-auto'>
         {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
-
-
         </div>
-
      </div>
-      
+     <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <div className='flex overflow-auto'>
+        {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+     </div>
     </>
   )
 }
