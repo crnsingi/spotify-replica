656cc8fcd966d124e2930727be228ddf5937b957
Cesar <cesarnsingi@hotmail.com>
2024-10-17 10:49:25 +0400

    add Display menu and properties

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='w-full flex justify-between items-center font-semibold'>
+        <div className='flex items-center gap-2'>
+            <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
+
+        </div>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default Navbar
