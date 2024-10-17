e73ab3e9b8bffbde2bd3b08d16d93e8257a2d2a1
Cesar <cesarnsingi@hotmail.com>
2024-10-17 12:11:20 +0400

    add A text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,12 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
             <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
             <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
-            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>D</p>
+            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>C</p>
 
         </div>
+      </div>
+      <div className='flex items-center gap-2 mt-4'>
+        <p>All</p>
 
       </div>
       
