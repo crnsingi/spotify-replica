7fca268d1183a27e318ed8dce3ae5757ebd5a731
Cesar <cesarnsingi@hotmail.com>
2024-10-17 16:30:20 +0400

    create songItem

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
@@ -14,9 +15,9 @@ const DisplayHome = () => {
         </div>
      </div>
      <div className='mb-4'>
-        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
         <div className='flex overflow-auto'>
-        {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            {}
         </div>
      </div>
     </>
