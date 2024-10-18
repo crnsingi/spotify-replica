580609b76748e2fc45f65638c5df17c47b82086d
Cesar <cesarnsingi@hotmail.com>
2024-10-18 09:38:48 +0400

    find album data

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useParams } from 'react-router-dom';
+import { albumsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
 
     const {id} = useParams();
+    const albumsData = albumsData[id];
 
   return (
     <>
     <Navbar/>
-      
     </>
   )
 }
