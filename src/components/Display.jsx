ba301375c23c7fbe1f562be68bfe8ac301200da4
Cesar <cesarnsingi@hotmail.com>
2024-10-18 20:40:38 +0400

    play with id

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
 
