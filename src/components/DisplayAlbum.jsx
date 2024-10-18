745a086a606d19da2bdaafd017a2231656eb38dc
Cesar <cesarnsingi@hotmail.com>
2024-10-18 09:52:11 +0400

    fix typing error

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -6,14 +6,13 @@ import { albumsData } from '../assets/assets';
 const DisplayAlbum = () => {
 
     const {id} = useParams();
-    const albumsData = albumsData[id];
+    const albumData = albumsData[id];
 
   return (
     <>
     <Navbar/>
     <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-        <img src={albumsData.image} alt="" />
-
+        <img src={albumData.image} alt="" />
     </div>
     </>
   )
