eaf116a897a1c040919bb292c104af3230ba3426
Cesar <cesarnsingi@hotmail.com>
2024-10-18 08:59:31 +0400

    create music algum page

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,9 +7,8 @@ const Display = () => {
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome/>} />
-
+            <Route path='/' element={<DisplayHome/>} />
         </Routes>
-      
     </div>
   )
 }
