7b979e7d4ccb526a925e632a075128e7af7e5625
Cesar <cesarnsingi@hotmail.com>
2024-10-18 14:32:06 +0400

    create navigation button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <>
       <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
-            <img onClick={()=>navigate(+1)} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
+            <img onClick={()=>navigate(-1)} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
             <img onClick={()=>navigate(1)} className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
         </div>
         <div className='flex items-center gap-4'>
